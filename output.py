847cb2134d145e2607c58355f7cfbd7504f193c0
Daniil123Rus <95186629+Daniil123Rus@users.noreply.github.com>
2023-02-05 20:48:10 +0600

    Update output.py

--- output.py
+++ output.py
@@ -1,6 +1,6 @@
 #This code was generated using Morse Compiler 0.1 (MorseLang -> Python 3)
 #You can use the code for your own purposes without restrictions.
-#Any questions? https://t.me/daniil_backend_developer
+#Any questions? https://github.com/Daniil123Rus/Morse/issues | https://t.me/daniil_backend_developer
 #Thanks for the help: PansanGG_
 
 print("hello world")
